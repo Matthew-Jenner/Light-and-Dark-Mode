3919e25c06751cc3e7d726242ac774e7466fe33c
Matthew-Jenner <121139184+Matthew-Jenner@users.noreply.github.com>
2023-12-08 14:03:51 +0000

    dark and light mode added dynamically

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-const toggleSwitch = document.querySelector('input[type="checkbox"]')
-const nav = document.getElementById('nav')
-const toggleIcon = document.getElementById('toggle-icon')
-const image1 = document.getElementById('image1')
-const image2 = document.getElementById('image2')
-const image3 = document.getElementById('image3')
-const textBox = document.getElementById('text-box')
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
 
+// dark or light images
+function imageMode(color) {
+	image1.src = ` img/undraw_business_chat_re_gg4h_${color}.svg`;
+	image2.src = ` img/undraw_online_test_re_kyfx_${color}.svg`;
+	image3.src = ` img/undraw_team_effort_yj7_${color}.svg`;
+}
 
 //  Dark Mode Styles
-function darkMode(){
-nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-toggleIcon.children[0].textContent = 'Dark Mode'
-toggleIcon.children[1].classList.remove('fa-sun')
-toggleIcon.children[1].classList.add('fa-moon')
-image1.src = 'img/undraw_business_chat_re_gg4h_dark.svg'
-image2.src = 'img/undraw_online_test_re_kyfx_dark.svg'
-image3.src = 'img/undraw_team_effort_yj7m_dark.svg'
+function darkMode() {
+	nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+	textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+	toggleIcon.children[0].textContent = "Dark Mode";
+	toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+	imageMode("dark");
 }
 // light mode styles
-function lightMode(){
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.remove('fa-moon')
-    toggleIcon.children[1].classList.add('fa-sun')
-    image1.src = 'img/undraw_business_chat_re_gg4h_light.svg'
-    image2.src = 'img/undraw_online_test_re_kyfx_light.svg'
-    image3.src = 'img/undraw_team_effort_yj7_light.svg'
-    }
-
-
+function lightMode() {
+	nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+	textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+	toggleIcon.children[0].textContent = "Light Mode";
+	toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+	imageMode("light");
+}
 
 // swtich theme dynamically
 function switchTheme(event) {
-    if (event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark')
-        darkMode()
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light')
-        lightMode()
-    }
+	if (event.target.checked) {
+		document.documentElement.setAttribute("data-theme", "dark");
+		darkMode();
+	} else {
+		document.documentElement.setAttribute("data-theme", "light");
+		lightMode();
+	}
 }
 // Event Listener
 
-toggleSwitch.addEventListener('change', switchTheme)
+toggleSwitch.addEventListener("change", switchTheme);
